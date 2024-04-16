63fcfa5042f9ffad571557fe4fd84b87a1dad194
TickTackk <me@ticktackk.io>
2024-04-16 16:37:48 +0530

    closes #50

--- Listener.php
+++ Listener.php
@@ -2,12 +2,16 @@
 
 namespace TickTackk\SignatureOnce;
 
+use TickTackk\SignatureOnce\Entity\ContentTrait as EntityContentTrait;
+use TickTackk\SignatureOnce\XF\Entity\Post as ExtendedPostEntity;
+use XF\Entity\User as UserEntity;
+use TickTackk\SignatureOnce\XF\Entity\UserOption as ExtendedUserOptionEntity;
 use XF\Service\User\DeleteCleanUp as UserDeleteCleanUpSvc;
 use XF\Service\User\ContentChange as UserContentChangeSvc;
 
 /**
  * @since 2.0.0
- * @version 2.0.2
+ * @version 2.0.6
  */
 class Listener
 {
@@ -46,5 +50,66 @@ class Listener
     {
         // this is combined with queries in \TickTackk\SignatureOnce\XF\Service\User\ContentChange::stepMergeContainerFirstUserContentForTckSignatureOnce() to do a "merge"
         $updates['xf_tck_signature_once_container_first_user_content'] = ['user_id'];
+    }
+
+    /**
+     * @since 2.0.6
+     *
+     * Allows the modification of various properties for template macros before they are rendered.
+     *
+     * Event hint: A string representing the template type, template name and macro name, e.g. public:template_name:macro_name.
+     *
+     * @param \XF\Template\Templater $templater Templater object.
+     * @param mixed $type Template type.
+     * @param mixed $template Template name.
+     * @param mixed $name Macro name.
+     * @param array $arguments Array of arguments passed to this macro.
+     * @param array $globalVars Array of global vars available to this macro.
+     *
+     * @noinspection PhpUnusedParameterInspection
+     *
+     * @throws \Exception
+     */
+    public static function templaterMacroPreRender(
+        \XF\Template\Templater $templater,
+        &$type,
+        &$template,
+        &$name,
+        array &$arguments,
+        array &$globalVars
+    ): void
+    {
+        /** @var EntityContentTrait $content */
+        $content = null;
+        $user = null;
+
+        switch ("$type:$template:$name")
+        {
+            case 'public:post_macros:post':
+                $content = $arguments['post'] ?? null;
+                if ($content instanceof ExtendedPostEntity)
+                {
+                    $user = $content->User;
+                }
+                break;
+
+            case 'public:conversation_message_macros:message':
+                $content = $arguments['message'] ?? null;
+                if ($content instanceof ExtendedPostEntity)
+                {
+                    $user = $content->User;
+                }
+                break;
+        }
+
+        if (($content !== null) && ($user instanceof UserEntity) && (method_exists($content, 'canShowSignature')))
+        {
+            if (!$user->Option->hasOption('tck_show_signature'))
+            {
+                return;
+            }
+
+            $user->Option->setOption('tck_show_signature', $content->canShowSignature());
+        }
     }
 }
